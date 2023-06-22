93651d0e61481342f586f08154d56b14598f539f
yoshiri <yoshiyoshidetteiu@gmail.com>
2023-06-22 19:05:56 +0900

    refactor utils
    Signed-off-by: yoshiri <yoshiyoshidetteiu@gmail.com>

--- tracking_validation/utils.py
+++ tracking_validation/utils.py
@@ -13,8 +13,15 @@ from tf_transformations import euler_from_quaternion
 # target objects
 from autoware_auto_perception_msgs.msg import TrackedObject, DetectedObject, PredictedObject
 
+# typing
+from typing import List
+from typing import Optional
+from typing import Tuple
+from typing import Union
 
-def getPosition(object):
+PerceptionObject = Union[TrackedObject, DetectedObject, PredictedObject]
+
+def getPosition(object: PerceptionObject) -> np.ndarray:
     """get pose from ros message
     """
     dtype = type(object)
@@ -27,12 +34,12 @@ def getPosition(object):
         exit(1)
     return np.array([pose.position.x, pose.position.y, pose.position.z])
 
-def get2DPosition(object):
+def get2DPosition(object: PerceptionObject):
     """get 2D pose from ros message
     """
     return getPosition(object)[:2]
     
-def get2DPositionCovarianceMatrix(object):
+def get2DPositionCovarianceMatrix(object: PerceptionObject) :
     """ get position covariance from ros message
     """
     dtype = type(object)
@@ -45,7 +52,7 @@ def get2DPositionCovarianceMatrix(object):
         exit(1)
     return np.array([pose_with_covariance.covariance[0],pose_with_covariance.covariance[1], pose_with_covariance.covariance[6], pose_with_covariance.covariance[7]])
 
-def get2DPositionCovariance(object):
+def get2DPositionCovariance(object: PerceptionObject) -> np.ndarray:
     """get diag x, y, yaw covariance from ros message
     """
     dtype = type(object)
@@ -58,7 +65,7 @@ def get2DPositionCovariance(object):
         exit(1)
     return np.array([pose_with_covariance.covariance[0],pose_with_covariance.covariance[7], pose_with_covariance.covariance[35]])
 
-def getVelocityCovariance(object):
+def getVelocityCovariance(object: PerceptionObject) -> np.ndarray:
     dtype = type(object)
     if dtype == TrackedObject or dtype == DetectedObject:
         twist_with_covariance = object.kinematics.twist_with_covariance
@@ -70,7 +77,7 @@ def getVelocityCovariance(object):
     return np.array([twist_with_covariance.covariance[0],twist_with_covariance.covariance[7], twist_with_covariance.covariance[14],
                     twist_with_covariance.covariance[21],twist_with_covariance.covariance[28], twist_with_covariance.covariance[35]])
 
-def getOrientation(object):
+def getOrientation(object: PerceptionObject) -> Tuple[float, float, float]:
     """get orientation from ros message
     """
     dtype = type(object)
@@ -83,12 +90,12 @@ def getOrientation(object):
         exit(1)
     return euler_from_quaternion([orientation.x, orientation.y, orientation.z, orientation.w], axes='sxyz')
 
-def getYaw(object):
+def getYaw(object: PerceptionObject) -> float:
     """get yaw from ros message
     """
     return getOrientation(object)[2]
 
-def getVX(object):
+def getVX(object: PerceptionObject) -> float:
     """get velocity x from ros message
     """
     dtype = type(object)
@@ -108,7 +115,8 @@ def getTimeFromStamp(stamp):
     except:
         return None
     
-def getLabel(obj):
+from autoware_auto_perception_msgs.msg import ObjectClassification
+def getLabel(obj: PerceptionObject) -> ObjectClassification:
     max_prob = 0
     label = None
     for cl in obj.classification:
@@ -136,10 +144,10 @@ def calc2DPolygonArea(footprints):
 
 
 # get transform from tf
-from rclpy.time import Time
+from rclpy.time import Time as rclpyTime
 def get_transform_from_tf(tf_buffer, child_frame_id: str, parent_frame_id: str, stamp):
     if type(stamp) == float:
-        target_time = Time(second = stamp)
+        target_time = rclpyTime(second = stamp)
     else:
         target_time = stamp
     try:
@@ -166,3 +174,14 @@ def get_pose_from_tf(tf_buffer, child_frame_id: str, parent_frame_id: str, stamp
     if transform == None:
         return None
     return get_pose_from_tansform(transform)
+
+## basic utils
+from builtin_interfaces.msg import Time
+def unixTimeUsToStamp(time: int) -> Time:
+    stamp = Time()
+    stamp.sec = int(time * 1e-6)
+    stamp.nanosec = int(time % 1e6 * 1e3)
+    return stamp
+
+def stampToUnixTimeUs(stamp: Time) -> int: 
+    return int(stamp.sec * 1e6 + stamp.nanosec * 1e-3)
